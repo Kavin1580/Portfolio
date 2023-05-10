0339fc2998779f04cd14ee9035c509bce0f7c862
Kavin <127176075+Kavin1580@users.noreply.github.com>
2023-05-10 13:36:58 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -2,10 +2,7 @@
 
 export const Work = [
     {
-        id:1,
-        name:"Shiash info pvt lmt",
-        description:"JAVA internship of 3 months",  
-        img:"https://media.licdn.com/dms/image/C5622AQFzKDKy542zAg/feedshare-shrink_800/0/1677414829428?e=1684972800&v=beta&t=a3zCU6Um8j0s6Yhqq6zNxfytRJsis5rnvTSatAlrXTo",
+      
     },
     
 
